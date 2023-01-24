982b078581de1900417c8d147cf8031704b69517
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-24 16:41:37 +0100

    Add internal data version

--- src/components/AppBoard.vue
+++ src/components/AppBoard.vue
@@ -1,5 +1,39 @@
 <script>
+import { getCurrentInstance } from 'vue';
+import PokemonCard from './pokemon/PokemonCard.vue';
 export default {
+    name: 'AppBoard',
+    components: { PokemonCard },
+    data() {
+        return {
+            pokemons: [
+                {
+                    id: 1,
+                    type1: 'green',
+                    number: 1,
+                    name: 'Bulbasaur',
+                    img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
+
+                },
+                {
+                    id: 2,
+                    type1: 'red',
+                    number: 4,
+                    name: 'Charmender',
+                    img: 'https://img.joomcdn.net/4391cf741f749de91359c2b37a4b153b0193394b_original.jpeg'
+
+                },
+                {
+                    id: 3,
+                    type1: 'blue',
+                    number: 7,
+                    name: 'Squirtle',
+                    img: 'https://img.joomcdn.net/7ff6debe2ecc1003e96dfda69e006418aef60a67_original.jpeg'
+
+                },
+            ]
+        }
+    }
 
 }
 </script>
@@ -7,7 +41,8 @@ export default {
 <template>
     <div class="container" id="main-board">
         <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5">
-
+            <pokemon-card v-for="pokemon in pokemons" :name="pokemon.name" :img="pokemon.img" :type="pokemon.type1"
+                :number="pokemon.number"></pokemon-card>
         </div>
     </div>
 </template>
---
+++ src/components/pokemon/PokemonCard.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: 'Pokemon List',
+    props: {
+        img: String,
+        name: String,
+        number: Number,
+        type: String
+    }
+
+}
+
+</script>
+
+<template>
+    <div class="col pokemon-card p-3 text-center ">
+        <div class="card-content text-white p-3" :style="`background-color:${type}`">
+            <img :src="img" :alt="name" class="img-fluid">
+            <h4 class="m-3">#{{ number }}</h4>
+            <h3>{{ name }}</h3>
+            <div class="type">{{ type }}</div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.pokemon-card {
+
+    .card-content {
+        border-radius: 20px;
+
+        img {
+            border-radius: 50%;
+        }
+
+    }
+}
+</style>
