dc249b33c718674d2488781868a6f674c6fe7173
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-24 15:59:36 +0100

    Add axios,store.js

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script>
+import axios from 'axios';
+import { store } from './data/store';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
 export default {
-
+  name: 'App pokedex',
+  data() {
+    return { store }
+  },
   components: { AppHeader, AppMain },
+  created() {
+    axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=1')
+      .then(res => {
+        store.pokemons = res.data.docs;
+      });
+  }
 
 }
 
