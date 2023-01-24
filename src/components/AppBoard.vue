8823c69d0681a9e399b861ab485be940100337d8
MarcoCalabretta1988 <calabretta.mar@gmail.com>
2023-01-24 17:16:01 +0100

    Add import data to API

--- src/components/AppBoard.vue
+++ src/components/AppBoard.vue
@@ -1,38 +1,11 @@
 <script>
-import { getCurrentInstance } from 'vue';
+import { store } from '../data/store';
 import PokemonCard from './pokemon/PokemonCard.vue';
 export default {
     name: 'AppBoard',
     components: { PokemonCard },
     data() {
-        return {
-            pokemons: [
-                {
-                    id: 1,
-                    type1: 'green',
-                    number: 1,
-                    name: 'Bulbasaur',
-                    img: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png'
-
-                },
-                {
-                    id: 2,
-                    type1: 'red',
-                    number: 4,
-                    name: 'Charmender',
-                    img: 'https://img.joomcdn.net/4391cf741f749de91359c2b37a4b153b0193394b_original.jpeg'
-
-                },
-                {
-                    id: 3,
-                    type1: 'blue',
-                    number: 7,
-                    name: 'Squirtle',
-                    img: 'https://img.joomcdn.net/7ff6debe2ecc1003e96dfda69e006418aef60a67_original.jpeg'
-
-                },
-            ]
-        }
+        return { store }
     }
 
 }
@@ -41,8 +14,9 @@ export default {
 <template>
     <div class="container" id="main-board">
         <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5">
-            <pokemon-card v-for="pokemon in pokemons" :key="pokemons.id" :name="pokemon.name" :img="pokemon.img"
-                :type="pokemon.type1" :number="pokemon.number"></pokemon-card>
+            <pokemon-card v-for="pokemon in store.pokemons" :key="store.pokemons.id" :name="pokemon.name"
+                :img="pokemon.imageUrl" :color="pokemon.color" :type="pokemon.type1"
+                :number="pokemon.number"></pokemon-card>
         </div>
     </div>
 </template>
--- src/components/pokemon/PokemonCard.vue
+++ src/components/pokemon/PokemonCard.vue
@@ -5,7 +5,8 @@ export default {
         img: String,
         name: String,
         number: Number,
-        type: String
+        color: String,
+        type: String,
     }
 
 }
@@ -14,10 +15,11 @@ export default {
 
 <template>
     <div class="col pokemon-card p-3 text-center ">
-        <div class="card-content text-white p-3" :style="`background-color:${type}`">
-            <img :src="img" :alt="name" class="img-fluid">
-            <h4 class="m-3">#{{ number }}</h4>
-            <h3>{{ name }}</h3>
+        <div class="card-content text-white py-3" :style="`background-color:${color}`">
+
+            <img :src="img" :alt="name">
+            <h5 class="m-2">#{{ number }}</h5>
+            <h4>{{ name }}</h4>
             <div class="type">{{ type }}</div>
         </div>
     </div>
@@ -28,11 +30,17 @@ export default {
 
     .card-content {
         border-radius: 20px;
+        height: 300px;
+        border: 1px solid black;
 
         img {
+            border: 1px solid black;
             border-radius: 50%;
+            width: 150px;
+            height: 150px;
         }
-
     }
+
+
 }
 </style>
